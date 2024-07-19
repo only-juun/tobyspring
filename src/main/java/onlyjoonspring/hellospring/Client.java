79d388723da12268bfad9fdacbe6404f293fc8e2
onlyjoon <nhj7334@icloud.com>
2024-07-19 23:34:33 +0900

    섹션 2 오브젝트와 의존관계

---
+++ src/main/java/onlyjoonspring/hellospring/Client.java
@@ -0,0 +1,12 @@
+package onlyjoonspring.hellospring;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        PaymentService paymentService = new PaymentService(new SimpleExRateProvider());
+        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+        System.out.println(payment);
+    }
+}
---
+++ src/main/java/onlyjoonspring/hellospring/ExRateProvider.java
@@ -0,0 +1,8 @@
+package onlyjoonspring.hellospring;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+public interface ExRateProvider {
+    BigDecimal getExRate(String currency) throws IOException;
+}
