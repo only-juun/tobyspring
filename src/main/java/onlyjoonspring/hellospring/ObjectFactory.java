64d20ff107397beb87eb044b3153203dfc723197
남환준 <nhj7334@icloud.com>
2024-07-20 15:54:32 +0900

    섹션 2 오브젝트와 의존관계: 싱글톤 레지스트리

--- src/main/java/onlyjoonspring/hellospring/ObjectFactory.java
+++ src/main/java/onlyjoonspring/hellospring/ObjectFactory.java
@@ -1,19 +1,30 @@
 package onlyjoonspring.hellospring;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
 public class ObjectFactory {
-//    @Bean
-//    public PaymentService paymentService() {
-//        return new PaymentService(exRageProvider());
-//    }
-//
-//    @Bean
-//    public ExRateProvider exRageProvider() {
-//        return new SimpleExRateProvider();
-//    }
+    @Bean
+    public PaymentService paymentService() {
+        return new PaymentService(exRageProvider());
+    }
+
+    @Bean
+    public OrderService orderService() {
+        return new OrderService(exRageProvider());
+    }
+
+    @Bean
+    public ExRateProvider exRageProvider() {
+        return new SimpleExRateProvider();
+    }
+}
+
+class OrderService {
+    ExRateProvider exRateProvider;
+
+    public OrderService(ExRateProvider exRateProvider) {
+        this.exRateProvider = exRateProvider;
+    }
 }
