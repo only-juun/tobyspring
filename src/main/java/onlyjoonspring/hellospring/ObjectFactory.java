8f6fd0bf945244599653a57e551fef5baf5f3873
남환준 <nhj7334@icloud.com>
2024-07-20 14:56:08 +0900

    섹션 2 오브젝트와 의존관계: 스프링 컨테이너와 의존관계 주입

--- src/main/java/onlyjoonspring/hellospring/ObjectFactory.java
+++ src/main/java/onlyjoonspring/hellospring/ObjectFactory.java
@@ -1,11 +1,17 @@
 package onlyjoonspring.hellospring;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ObjectFactory {
+    @Bean
     public PaymentService paymentService() {
         return new PaymentService(exRageProvider());
     }
 
+    @Bean
     public ExRateProvider exRageProvider() {
-        return new WebApiExRateProvider();
+        return new SimpleExRateProvider();
     }
 }
