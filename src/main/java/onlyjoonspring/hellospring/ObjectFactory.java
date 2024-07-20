f84f060f1d9e0991ed74dfef594b0fd5dc356077
남환준 <nhj7334@icloud.com>
2024-07-20 15:25:58 +0900

    섹션 2 오브젝트와 의존관계: 구성정보를 가져오는 다른 방법

--- src/main/java/onlyjoonspring/hellospring/ObjectFactory.java
+++ src/main/java/onlyjoonspring/hellospring/ObjectFactory.java
@@ -1,17 +1,19 @@
 package onlyjoonspring.hellospring;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 public class ObjectFactory {
-    @Bean
-    public PaymentService paymentService() {
-        return new PaymentService(exRageProvider());
-    }
-
-    @Bean
-    public ExRateProvider exRageProvider() {
-        return new SimpleExRateProvider();
-    }
+//    @Bean
+//    public PaymentService paymentService() {
+//        return new PaymentService(exRageProvider());
+//    }
+//
+//    @Bean
+//    public ExRateProvider exRageProvider() {
+//        return new SimpleExRateProvider();
+//    }
 }
